f329b22afd43e6d60ef592f0f31faf5793040380
Isaac <hyperneato@gmail.com>
2012-03-04 00:03:50 -0500

    Better help message. Easier to use at the commandline.

--- timer.py
+++ timer.py
@@ -9,19 +9,28 @@ if __name__ == "__main__":
     from time import sleep
     import subprocess
     import argparse
-    parser = argparse.ArgumentParser(description='''Set an audible timer for -m minutes 
-and/or -s seconds.\n$python timer.py -m 5 # after 5 minutes execute a subprocess
-to play some audio.\nThe function or method of generating audio must be configured
-within this script.''')
-    parser.add_argument('--s', type=int, help='number of seconds',
-                        required=False)
-    parser.add_argument('minutes', help='number of minutes', type=int)
-    
-    parser.set_defaults(minutes=5, s=0)
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description=
+'''Set a countdown timer, then execute a subprocess to play some audio. 
+The function or method of generating audio must be configured within this script.
+
+Usage:
+
+    $ python timer.py 5           # 5 minute timer 
+    $ python timer.py 0 -s 30     # 30 second timer
+    $ python timer.py -s 999 0    # 999 second timer
+''')
+
+    parser.add_argument('-s', type=int, help='<the number of seconds to countdown>',
+                        required=False, metavar='seconds')
+    parser.add_argument('minutes', help='<the number of minutes to countdown>', type=int)
+                        
+
+    parser.set_defaults(s=0)
     args = parser.parse_args()
 
     total_seconds = (args.minutes * 60) + args.s
     sleep(total_seconds)
 
-    # how I generate sound
-    subprocess.call('chuck moe.ck', shell=True)
+    # how I personally generate sound
+    subprocess.call('/Users/me/bin/chuck /Users/me/chuck/alarms/moe.ck', shell=True)
